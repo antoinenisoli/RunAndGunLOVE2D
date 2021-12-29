38107cfb829d185630a54f0ab307f51ce736efb9
Antoine Nisoli <nisoliantoine@outlook.com>
2021-12-29 14:52:53 +0100

    first animations

--- main.lua
+++ main.lua
@@ -1,13 +1,19 @@
 love.graphics.setDefaultFilter('nearest', 'nearest')
 
 function love.load()
-    -- init something here ...
+    anim8 = require 'libraries/anim8'
+    
     player = {}
     player.x = 250
     player.y = 250
     player.speed = 2
     player.sprite = love.graphics.newImage('assets/sprites/tile000.png')
     player.spriteSheet = love.graphics.newImage('assets/sprites/SpriteSheet_player.png')
+    player.grid = anim8.newGrid( 45, 45, player.spriteSheet:getWidth(), player.spriteSheet:getHeight() )
+
+    player.animations = {}
+    player.animations.down = anim8.newAnimation(player.grid('1-4', 1), 0.2)
+    player.animations.idle = anim8.newAnimation(player.grid('1-6', 1), 0.2)
 
     love.window.setTitle('Hello love2d!')
     love.keyboard.keysPressed = {}
@@ -51,11 +57,13 @@ function love.update(dt)
     end
 
     love.keyboard.keysPressed = {}
+    player.animations.idle:update(dt)
 end
 
 function love.draw()
     -- draw your stuff here
     love.graphics.print('Welcome to the Love2d world!', 10, 10)
-    love.graphics.draw(player.sprite, player.x, player.y)
+    --love.graphics.draw(player.sprite, player.x, player.y)
+    player.animations.idle:draw(player.spriteSheet, player.x, player.y, nil, 1)
 end
 
